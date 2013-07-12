e3e5b7e06aa441171ee976661b17de0baf502f0a
Marcel Fahle <m.fahle@gmail.com>
2013-07-12 13:24:53 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularCasts::Application.config.secret_key_base = '3bf13428a0e6d15ac0ffd8b17a443312b35b60cb9f8502ec771022d7f28e986c546b47b36efe3539fff6b1b2abf9dc81deafd9e863482061fa3204e3d0757cdf'
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+AngularCasts::Application.routes.draw do
+
+  scope :api do
+    get "/screencasts(.:format)" => "screencasts#index"
+    get "/screencasts/:id(.:format)" => "screencasts#show"
+  end
+  root to: "home#index"
+
+end
